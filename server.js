5b155d574461394f552725acbb64935f248b4cdf
Diego Armando <Edu.Diegoarmando@hotmail.com>
2018-09-18 15:22:32 -0300

    routes defined

--- server.js
+++ server.js
@@ -37,6 +37,18 @@ app.get('/', (req, res) =>
   res.send(path.join(__dirname, '/public', 'index.html'))
   );
 
+app.get('/url-shortener/:urlx', async (req, res) => {
+  const { urlx } = req.params;
+  console.log(urlx);
+  const arrayquer = await Url.find({ shortenedUrl: urlx });
+  if ( arrayquer.length !== 0 ){
+    const { originalUrl } = arrayquer[0];
+    res.redirect(originalUrl)
+  } else {
+    res.json( { "error" : "No short url found for given input" } );
+  }
+});
+
 app.post('/url-shortener', async function (req, res) {{
 
   const { url_name } = req.body;
@@ -48,18 +60,22 @@ app.post('/url-shortener', async function (req, res) {{
   '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
   '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
 
-  //usar o regexp para validar a url, depois usar o dns para verificar se o site existe
-  //caso as duas condições sejam satisfeitas retorne o json com o a Original e a Short
-
-  if(pattern.test( url_name ) === true){ //caso não passe no regex ele retorna um json com o erro
-    //Invalid Url regex Error
-    urlExists(url_name , async function(err, exists) { //Aqui verifica se a Url existe, caso não
-      //exista retorna um json com o erro URL does not exist
+  if ( pattern.test( url_name ) === true ){
+    urlExists(url_name , async function(err, exists) {
       if(exists === true){
-        const all = await Url.find();
-        const createdUrl = await Url.create({ originalUrl: url_name, shortenedUrl: (all.length + 1) });
-        const {originalUrl, shortenedUrl} = createdUrl
-        res.json({originalUrl, shortenedUrl});
+        const arrayquer = await Url.find({ originalUrl: url_name });
+        if ( arrayquer.length !== 0 ){
+          const { originalUrl, shortenedUrl } = arrayquer[0];
+          res.json({originalUrl, shortenedUrl});
+          console.log("Já existe mano!!");
+        } else {
+          console.log("To registrando mano!!");
+          const all = await Url.find();
+          const createdUrl = await Url.create({ originalUrl: url_name, shortenedUrl: (all.length + 1) });
+          const {originalUrl, shortenedUrl} = createdUrl
+          res.json({originalUrl, shortenedUrl});
+        }
+        
       } else {
         res.json({ error : "Invalid Url, URL does not exist"});
       }
